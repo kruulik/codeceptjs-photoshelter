c2651bb6807a8376537d98c0762c37067bc4c10e
Karolis Krulis <karoliskrulis@gmail.com>
2017-04-03 20:26:33 -0400

    initial commit and updates

--- upload_test.js
+++ upload_test.js
@@ -3,4 +3,19 @@ Feature('Upload');
 
 Scenario('test something', (I) => {
 
+  I.amOnPage('http://www.photoshelter.com/mem/home/');
+  I.fillField('U_EMAIL', 'Kemail_test1@gmail.com');
+  I.fillField('U_PASSWORD', 'mypassword_test');
+  I.click('sign in');
+  I.wait('5');
+  I.click('//*[@id="navbar"]/ul[1]/li[3]');
+  //xpath for upload button is different on premium and trial user profiles
+  I.wait('1');
+  I.click('//*[@id="navbar"]/ul[1]/li[3]/ul/li[2]/a');
+  I.wait('1');
+  within('//*[@id="Dialog_ClcGal_Create"]', function(){
+    I.fillField('NAME', new Date().toLocaleDateString('en-US'));
+    I.pressKey('Enter');
+  });
+  I.attachFile('//*[@id="ubFile"]/div/input','image/Karolis.gif')
 });
